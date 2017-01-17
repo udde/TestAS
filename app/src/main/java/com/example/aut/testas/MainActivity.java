2b6d7d525591e2671a9e66854bb55dac9f9064a4
Alexander Uddfeldt <alexander.uddfeldt@gaia.se>
2017-01-17 11:50:16 +0100

    _

--- app/src/main/java/com/example/aut/testas/MainActivity.java
+++ app/src/main/java/com/example/aut/testas/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.aut.testas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import com.microsoft.azure.mobile.MobileCenter;
+import com.microsoft.azure.mobile.analytics.Analytics;
+import com.microsoft.azure.mobile.crashes.Crashes;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //mobile center
+        MobileCenter.start(getApplication(), "6f3a4d53-1291-40ec-8b4c-db7fea18990e",
+                Analytics.class, Crashes.class);
+
         setContentView(R.layout.activity_main);
     }
 }
